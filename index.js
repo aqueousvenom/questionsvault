126f1d5a5708445e3a37c1ce790890c104e53c52
aqueousvenom <157819515+aqueousvenom@users.noreply.github.com>
2024-12-14 16:36:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function sem1(num) {
+  var branch = document.getElementById('sBranch').value;
+  changeUrl(branch, num)
+}
+let url;
+function changeUrl(branch, num){
+const iframe = document.getElementById('dynamicIframe')
+
+if (branch == computer-science && num == 1) {
+  url = 'www.google.com';
+}
+
+iframe.href = url;
+}
